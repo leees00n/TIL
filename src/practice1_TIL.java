56022a733556598bce5296afeeaf10d60929a5d5
leees00n <leees0053@gmail.com>
2022-07-21 14:34:34 +0900

    Practice

---
+++ src/practice1_TIL.java
@@ -0,0 +1,25 @@
+public class practice1_TIL {
+    public static void solution(int num) {
+        int numReverse=0;
+        boolean isMinus = false;
+
+        if (num<0){
+            isMinus = true;
+            numReverse *= 1;
+        }
+
+        while(num>0){
+            int r = num % 10;
+            num /= 10;
+            numReverse = numReverse * 10 + r;
+        }
+        System.out.println(isMinus ?  numReverse * -1 : numReverse);
+    }
+    public static void main(String[] args) {
+        // Test code
+        solution(12345);
+        solution(-12345);
+        solution(100);
+        solution(0);
+    }
+}
